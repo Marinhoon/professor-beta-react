247b6b21600ce7fbabed0ef0da2b2cd86cb285ab
Vitor Marinho <vitormarinhoh@gmail.com>
2024-01-20 10:54:28 -0300

    ajuste da barra inicial

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logopb.png'
 
 const Header: React.FC = () => {
   return (
     <header>
-      <h1>Professor Beta</h1>
-      <img src="/path/to/your/logo.png" alt="Logo" className="logo" />
+       <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+
+      </div>
+      
+      <nav>
+        <ul>
+          <li><a href="#inicio">Início</a></li>
+          <li><a href="#servicos">Serviços</a></li>
+          <li><a href="#aulas">Aulas/Treinamentos</a></li>
+          <li><a href="#contato">Contato</a></li>
+        </ul>
+      </nav>
+      <img src="/path/to/your/logo.png" alt="" className="" />
     </header>
   );
 };
 
 export default Header;
+
+
